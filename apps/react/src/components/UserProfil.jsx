aeffc086621aa4207ff752a89611fbf52476d78e
John DYER <jdyer@e3r3p11.clusters.42paris.fr>
2022-06-06 15:59:29 +0200

    ajout bouton logout / login sur Home

--- apps/react/src/components/UserProfil.jsx
+++ apps/react/src/components/UserProfil.jsx
@@ -1,32 +1,60 @@
-/* aurelie */
+/*aurelie john*/
 
 import axios from 'axios';
 import React, { useEffect, useState } from 'react';
+import Login from "../pages/Login";
 
 const UserProfil = () => {
 
   //const[isLoading, setLoading] = useState(true);
     const [profil, setProfil] = useState([]);
-    
+    const [connected, setConnected] = useState([false]);
+
     useEffect(() => {
-     axios.get("http://localhost:3000/users", { withCredentials:true }).then((res) =>{ 
-    console.log(res.data);
-     setProfil(res.data);
+      axios.get("http://localhost:3000/users", { withCredentials:true }).then((res) =>{ 
+     console.log('wtch');
+     console.log(res.data);
+      setProfil(res.data);
+      setConnected(true);
     //  setLoading(false)
-   })
+    })
   }, [])
   
+  const handleClick = event => {
+    if (connected) {
+      axios.get("http://localhost:3000/auth/logout", { withCredentials:true })
+      //window.location.href="http://localhost:3000/auth/login";
+      setConnected(false);
+    }
+    else {
+      setConnected(true);
+      window.location.href="http://localhost:3000/auth/login";
+    }
+  };
+
   //if(isLoading) {
   //  return <div>wait for charging...</div>;
   // }
-  return(
-    <div>
-      <h1>Profile Page</h1>
-      
-  <div>{profil.login}</div>
-    
-    </div>
-  );
+  if (connected) {
+    console.log('cmt');
+    return(
+      <div>
+        <h1>Profile Page</h1>
+    <button onClick={handleClick}>
+          Logout
+        </button>
+    <div>{profil.login}</div>
+
+      </div>
+    ); }
+  else {
+    return(
+      <div>
+    <button onClick={handleClick}>
+          Login
+        </button>
+      </div>
+    ); }
   }
   // <div>{profil.login}</div>
   // <div>{profil.email}</div>
