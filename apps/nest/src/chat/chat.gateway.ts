0c321af1f2a7d055afe09577dcd62765d35c5c5c
John DYER <jdyer@e1r5p13.42.fr>
2022-06-09 14:28:40 +0200

    debut implementation salons

--- apps/nest/src/chat/chat.gateway.ts
+++ apps/nest/src/chat/chat.gateway.ts
@@ -15,7 +15,6 @@ export class ChatGateway implements OnGatewayConnection, OnGatewayDisconnect {
     // we then can use this to trigger events and send data to connected clients
     @WebSocketServer() server;
     users: number = 0;
-    
 
 
     // The handle connection hooks will keep track of clients connections and disconnection
@@ -35,12 +34,21 @@ export class ChatGateway implements OnGatewayConnection, OnGatewayDisconnect {
         this.server.emit('users', this.users);
     }
 
-    // this decorator is used to listenning incomming messages.
+    // this decorator is used to listenning incomming messages. chat channel
     @SubscribeMessage('chat')
-    // param 'client' will be a reference to the socket instance, param 'message' will be te data sent by the client
-    async onChat(client, message) {
-        //any clients listenning  for the cht event would receivethis data instantly
-        //client.broadcast.emit('chat', message);
-        this.server.emit('chat', message);
+    // param 'client' will be a reference to the socket instance, param 'data.p1' is the room where to emit, data.p2 is the message
+    async onChat(client, data) {
+        //any clients listenning  for the chat event on the data.p1 channel would receivethis data instantly
+        console.log('sendMessage');
+        this.server.emit(data.p1, data.p2);
+    }
+ 
+    @SubscribeMessage('addsalon')
+    // event d'ajout de salon
+    async addsalon(client, salon_to_add) {
+        //tous les users ecoutant le channel newsalon (donc tt le monde) recoivent l'ajout de salon
+        console.log(salon_to_add);
+        this.server.emit('newsalon', salon_to_add);
     }
 }
+
--- apps/react/src/components/Chat.js
+++ apps/react/src/components/Chat.js
@@ -18,11 +18,11 @@ const chatStyle = {
 
 const Chat = () => {
 
-  const [message, setMessage] = useState([]);// Tous les users de la db
-  const [newData, setNewData] = useState([]);// Tous les users de la db
+  const [message, setMessage] = useState([]);// Message a envoyer au salon
   const [users, setUsers] = useState([]);// Tous les users de la db
   const [userFound, setUserFound] = useState([]); //Contient l'utilisateur si trouve
-
+  const [currentSalon, setCurrentSalon] = useState(['chat']);// Salon courant
+  const [salons, setSalons] = useState(['chat']); //Array de tous les salons a afficher, que l'on peut selectionner
   /* Recupere tout les utilisateur, 1x slmt (componentDidMount) */
   useEffect(() => {
     axios.get("http://localhost:3000/users/all", { withCredentials: true }).then((res) => {
@@ -31,22 +31,53 @@ const Chat = () => {
 
   }, [])
 
+  //Definit le salon que l'on ecoute, hooked sur currentSalon, pour reactualiser le salon d'ecoute a chqaue changement de salon
+  //Ecoute le salon courant pour afficher tout nouveaux messages
   useEffect(() => {
-    socket.on("chat", data => {
+    console.log(currentSalon);
+    socket.on(currentSalon, data => {
+      console.log(currentSalon);
       setMessage((message) => {
         //data = {'\n'} + data;
         return ([...message, data]); });
       console.log(socket.id);
      });
-    }, [])
+    }, [currentSalon])
 
-
+    //Ecoute sur le channel newsalon pour ajouter les salons lorsqu'un utilisateur en cree
+    useEffect(() => {
+      socket.on('newsalon', data => {
+        console.log(data);
+        setSalons((salons) => {
+          return ([...salons, data]); });
+       });
+      }, [])
+  
+  //Emit le message rentre par l'utilisateur a tout le salon
   const sendMessage = (event) => {
     if(event.key === 'Enter') {
-      socket.emit('chat', event.target.value);
+      console.log(currentSalon);
+
+      socket.emit('chat', {p1: currentSalon, p2: event.target.value});
       event.target.value = "";
     }
   }
+
+  //emit les alon cree par l'utilisateur a tous les autres utilisateurs
+  const sendNewSalon = (event) => {
+    if(event.key === 'Enter') {
+      console.log(event.target.value)
+      socket.emit('addsalon', event.target.value);
+      event.target.value = "";
+    }
+  }
+
+  //handle l'evenement changement de salon quand l'utilisateur clique pour changer de salon
+  //ferme connection sur le channel de l'ancier salon, le setCurrentSalon trigger le useEffect qui va faire ecouter l'utilisateur sur le nouveau salon
+  const handleClick = (salon) => {
+    socket.off(currentSalon);
+    setCurrentSalon(salon);
+  };
 
 
   /* A chaque changement dans linput, verifie si trouve le user */
@@ -71,7 +102,17 @@ const Chat = () => {
       </div>
       <p>{userFound.login}</p>
       <p>{JSON.stringify(userFound)}</p>
+      {/* Affichage de l'array Salons par iteration */}
+      {salons.map((salon) => ( 
+      <button onClick={() => handleClick(salon)}>
+          {salon}
+        </button>))}
+        {/* Barre d'input pour ajouter un salon */}
+      <div>"Add a salon"</div>  
+        <input type='text' onKeyPress={sendNewSalon} />
+        {/* Barre d'input pour ajouter un message */}
       <input type='text' onKeyPress={sendMessage} />
+      {/* Affichage de la variable message detenant tout l'historique des messages*/}
       {message.map((msg) => (
         <div>{msg}</div>
       ))}
