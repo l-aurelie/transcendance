9b1631a76c16b89d4eacd4974ecd7cb7f4f68438
John DYER <jdyer@e1r2p13.clusters.42paris.fr>
2022-07-25 21:15:23 +0200

    encore

--- apps/nest/src/auth/interface/requestWithUser.interface.ts
+++
@@ -1,8 +0,0 @@
-import {Request} from 'express';
-import IUser from '../../typeorm/entities/User';
-
-interface RequestWithUser extends Request {
-    user: IUser;
-  }
-  
-  export default RequestWithUser;
--- apps/nest/src/auth/services/auth/auth.ts
+++
@@ -1,8 +0,0 @@
-import { UserDetails } from "src/auth/utils/types";
-import { User } from "src/typeorm";
-
-export interface AuthenticationProvider { //interface, pernmet de simplifier l'accessibilite depuis d'autre fichie
-    validateUser(details: UserDetails);
-    createUser(details: UserDetails);
-    findUser(intraId: string) : Promise<User> | undefined;
-}
